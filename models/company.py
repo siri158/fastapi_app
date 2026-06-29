ea54f1131830d3453691149c054dfb34fd6b2dce
siri <4al23ai054@aiet.org.in>
2026-06-29 11:40:23 +0530

    add location

--- models/company.py
+++ models/company.py
@@ -9,6 +9,6 @@ class Company(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True)
     phone = Column(String, unique=True)
-    location=Columns(string)
+    location=Column(String)
 
     jobs = relationship("Job", back_populates="company")
