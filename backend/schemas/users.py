0505d703261d903ed6df911a80a03facf05c2e2d
siri <4al23ai054@aiet.org.in>
2026-07-02 16:28:03 +0530

    backend

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,17 +1,23 @@
-
-
 from pydantic import BaseModel
 
 class UserBase(BaseModel):
-    name: str
-    email: str
-    role: str
+    name:str
+    email:str
+    password:str
+    role:str
 
 class UserCreate(UserBase):
-    password: str
+    pass
 
-class UserResponse(UserBase):
-    id: int
+class UserResponse(BaseModel):
+    id:int
+    name:str
+    email:str
+    role:str
 
     class Config:
-        from_attributes = True
+        from_attributes=True
+
+class Login_User(BaseModel):
+    email:str
+    password:str
